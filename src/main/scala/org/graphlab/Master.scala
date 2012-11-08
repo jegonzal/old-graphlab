dd7255dc3477c7f774602252726ee74d48d3ab82
Aapo Kyrola <aapo@protogeo.com>
2012-11-08 14:45:23 -0800

    started master in scala

---
+++ src/main/scala/org/graphlab/Master.scala
@@ -0,0 +1,12 @@
+package org.graphlab
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: akyrola
+ * Date: 11/8/12
+ * Time: 2:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Master {
+
+}
