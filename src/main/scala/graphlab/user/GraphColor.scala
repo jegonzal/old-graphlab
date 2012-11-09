d85da6ed6edeefb1827e48def30607b46d38788a
Carlos Guestrin <guestrin@graniteair.dyn.cs.washington.edu>
2012-11-09 11:29:20 -0800

    Graph coloring in Scala

---
+++ src/main/scala/graphlab/user/GraphColor.scala
@@ -0,0 +1,34 @@
+package graphlab.user
+
+import graphlab.user._
+import graphlab.engine._
+import scala.collection.mutable.Set
+
+object GraphColor {
+
+  def main(args:Array[String]) = {
+    
+    val m:Master[(Int,Int),Int,Set[Int]] = new Master
+
+    m.build_graph(args(0),(s)=>0,(id)=>(id,id))
+    
+    m.run_gas((v,e) => (e.data,
+        if(v.id > e.get_other_vertex(v).id) Set() else Set(e.get_other_vertex(v).data._1)
+        ), //gather
+      _|_, //sum
+      (v,g) => {
+    	  println("applying on vertex " + v.id + " with acc " + g)        	
+          var c = 0
+          while(g(c)) 
+            c += 1
+          println("Chose color " + c)
+          (c,v.data._1)
+      },	//apply
+      (v,e) => (e.data,v.data._1 != v.data._2),	//scatter
+      () => Set(), //init gather type
+      All,All) //gather_edges, scatter_edges
+
+    m.dump_graph()
+
+  }
+}
