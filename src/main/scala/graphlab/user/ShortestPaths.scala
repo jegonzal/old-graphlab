7a0ae02560be1a4660437ca3693bb2bfbbfd1e1b
Svilen <svilen.mihaylov@gmail.com>
2012-11-09 16:07:49 -0800

    fixing aapos screw up

---
+++ src/main/scala/graphlab/user/ShortestPaths.scala
@@ -0,0 +1,34 @@
+package graphlab.user
+
+import graphlab.user._
+import graphlab.engine._
+
+object ShortestPaths {
+
+  def main(args:Array[String]) = {
+    
+    val m:Master[(Double,Double),Double] = new Master
+
+    m.build_graph(args(0),(s)=>s.toDouble,(id)=>((if (id == args(1).toInt) 0.0 else -1.0), -1.0))
+    
+    def min_positive(x: Double, y: Double) = { 
+	  (x>y, x>=0.0, y>=0.0) match {
+	  case (true, _, true) => y
+	  case (true, _, false) => x
+	  case (false, true, true) => x
+	  case (_, false, true) => y
+	  case (false, _, false) => y
+	  }
+  }
+    
+    m.run_gas[Double]((v,e) => (e.data,e.get_other_vertex(v).data._1+e.data), //gather
+      min_positive, //sum
+      (v,g) => (min_positive(g,v.data._1),v.data._1),	//apply
+      (v,e) => (e.data,v.data._1 != v.data._2),	//scatter
+      -1.0, //init gather type
+      All,All) //gather_edges, scatter_edges
+
+    m.dump_graph()
+
+  }
+}
